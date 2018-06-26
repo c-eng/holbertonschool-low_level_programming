08a7dd262a1ade0289866286eaf1cd248af73fe9
c-eng <ceng@cca.edu>
2018-06-26 21:22:38 +0000

    Adding remaining mandatory tasks for testing

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds integers a and b
+ * @a: First integer to be evaluated
+ * @b: Second integer to be evaluated
+ *
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
