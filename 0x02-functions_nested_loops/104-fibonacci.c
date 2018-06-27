5ff6876b579a21b3eaeaa2a259dca9c6de3e545e
c-eng <ceng@cca.edu>
2018-06-27 04:27:55 +0000

    Fixing leading zeroes

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,37 +9,38 @@
 int main(void)
 {
 	long p2a = 0, p2b = 0, p2c = 0, p1a = 1, p1b = 0, p1c = 0;
-	long currenta = 0, currentb = 0, currentc = 0, counter;
+	long ca = 0, cb = 0, cc = 0, counter;
 
 	for (counter = 0 ; counter < 98 ; counter++)
 	{
-		currenta = (p1a + p2a) % 10000000;
+		ca = (p1a + p2a) % 10000000;
 		if ((p1a + p2a) / 10000000 > 0)
-			currentb = ((p1b + p2b) % 10000000) + 1;
+			cb = ((p1b + p2b) % 10000000) + 1;
 		else
-			currentb = (p1b + p2b) % 10000000;
+			cb = (p1b + p2b) % 10000000;
 		if ((p1b + p2b) / 10000000 > 0)
-			currentc = ((p1c + p2c) % 10000000) + 1;
+			cc = ((p1c + p2c) % 10000000) + 1;
 		else
-			currentc = (p1c + p2c);
+			cc = (p1c + p2c);
 		if (counter < 97)
 		{
-			if (currentc > 0)
-				printf("%ld", currentc);
-			if (currentb > 0)
-				printf("%ld", currentb);
-			printf("%ld, ", currenta);
+			if (cc > 0)
+				printf("%ld%07ld%07ld, ", cc, cb, ca);
+			else if (cb > 0)
+				printf("%ld%07ld, ", cb, ca);
+			else
+				printf("%ld, ", ca);
 		}
 		else
 		{
-			printf("%ld%ld%ld\n", currentc, currentb, currenta);
+			printf("%ld%ld%ld\n", cc, cb, ca);
 		}
 		p2a = p1a;
 		p2b = p1b;
 		p2c = p1c;
-		p1a = currenta;
-		p1b = currentb;
-		p1c = currentc;
+		p1a = ca;
+		p1b = cb;
+		p1c = cc;
 
 	}
 	return (0);
