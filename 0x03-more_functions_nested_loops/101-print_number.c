f5821d62b9fd5de3cc034fa311c9222dd0b3d607
c-eng <ceng@cca.edu>
2018-06-28 19:34:02 +0000

    Increasing digit limit to 7 digits

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -30,6 +30,10 @@ void print_number(int n)
 {
 	if (n < 0)
 		_putchar('-');
+	if (_abs(n) / 1000000 > 0)
+		_putchar(((_abs(n) / 1000000) % 10) + '0');
+	if (_abs(n) / 100000 > 0)
+		_putchar(((_abs(n) / 100000) % 10) + '0');
 	if (_abs(n) / 10000 > 0)
 		_putchar(((_abs(n) / 10000) % 10) + '0');
 	if (_abs(n) / 1000 > 0)
