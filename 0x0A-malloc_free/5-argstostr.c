3aa5603bc74d6453bcb8fc87beaa7859a13f9482
c-eng <ceng@cca.edu>
2018-07-18 06:12:16 +0000

    Commiting task for autochecking

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - concatanates args with \n seperators
+ * @ac: argument count
+ * @av: argument list
+ *
+ * Return: pointer or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int c, a_i, len = 0, cur_len = 0;
+	char *maomao;
+
+	if (ac > 1 && av)
+	{
+		for (c = 1 ; c < ac ; c++)
+		{
+			for (a_i = 0 ; av[c][a_i] != '\0' ; a_i++)
+				;
+			len += (a_i + 1);
+		}
+		len += 1;
+		maomao = malloc(sizeof(char) * len);
+		if (maomao)
+		{
+			for (c = 1 ; c < ac ; c++)
+			{
+				for (a_i = 0 ; av[c][a_i] != '\0' ; a_i++)
+				{
+					maomao[a_i + cur_len] = av[c][a_i];
+				}
+				maomao[a_i + cur_len] = '\n';
+				cur_len += (a_i + 1);
+			}
+			maomao[cur_len] = '\0';
+			return (maomao);
+		}
+	}
+	return ('\0');
+}
