89a72be6f00d87e8986422700d3eaa33cacd5b66
c-eng <ceng@cca.edu>
2018-07-20 04:44:14 +0000

    Fixing error due to not reading instructions

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -15,30 +15,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, len;
 	char *maomao;
 
-	if (s1 && s2)
+	if (!(s1))
+		s1 = "";
+	if (!(s2))
+		s2 = "";
+	for (len = 0 ; s1[len] != '\0' ; len++)
+		;
+	while (s2[i] != '\0' && i < n)
 	{
-		for (len = 0 ; s1[len] != '\0' ; len++)
-			;
-		while (s2[i] != '\0' && i < n)
+		len++;
+		i++;
+	}
+	maomao = malloc(sizeof(char) * (len + 1));
+	if (maomao)
+	{
+		for (i = 0 ; s1[i] != '\0' ; i++)
 		{
-			len++;
-			i++;
+			maomao[i] = s1[i];
 		}
-		maomao = malloc(sizeof(char) * (len + 1));
-		if (maomao)
+		len = i;
+		for (i = 0 ; s2[i] != '\0' && i < n ; i++)
 		{
-			for (i = 0 ; s1[i] != '\0' ; i++)
-			{
-				maomao[i] = s1[i];
-			}
-			len = i;
-			for (i = 0 ; s2[i] != '\0' && i < n ; i++)
-			{
-				maomao[len + i] = s2[i];
-			}
-			maomao[len + i] = '\0';
-			return (maomao);
+			maomao[len + i] = s2[i];
 		}
+		maomao[len + i] = '\0';
+		return (maomao);
 	}
 	return (NULL);
 }
