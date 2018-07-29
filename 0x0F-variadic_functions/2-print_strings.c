a57a8d1beaeaa25e2acca8b2c743390b9564e3cd
c-eng <ceng@cca.edu>
2018-07-29 00:30:54 +0000

    Adjusting for expected null output

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -16,20 +16,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list paramlist;
 	char *current;
 
-	if (n)
+	va_start(paramlist, n);
+	for (i = 0 ; i < n ; i++)
 	{
-		va_start(paramlist, n);
-		for (i = 0 ; i < n ; i++)
-		{
-			current = va_arg(paramlist, char *);
-			if (current)
-				printf("%s", current);
-			else
-				printf("(nil)");
-			if (separator && i != (n - 1))
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(paramlist);
+		current = va_arg(paramlist, char *);
+		if (current)
+			printf("%s", current);
+		else
+			printf("(nil)");
+		if (separator && i != (n - 1))
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(paramlist);
 }
