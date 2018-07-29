398cb654455785d16a3369fdeab17e4c7ae8ead1
c-eng <ceng@cca.edu>
2018-07-29 00:27:46 +0000

    Adjusting code for possible edge case

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -15,16 +15,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list paramlist;
 
-	if (n)
+	va_start(paramlist, n);
+	for (i = 0 ; i < n ; i++)
 	{
-		va_start(paramlist, n);
-		for (i = 0 ; i < n ; i++)
-		{
-			printf("%d", va_arg(paramlist, int));
-			if (separator && i != (n - 1))
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(paramlist);
+		printf("%d", va_arg(paramlist, int));
+		if (separator && i != (n - 1))
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(paramlist);
 }
