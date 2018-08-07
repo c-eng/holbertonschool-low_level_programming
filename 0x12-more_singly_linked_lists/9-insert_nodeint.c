2b94a155d60b5b1ba922117880b6efa893558e4c
c-eng <ceng@cca.edu>
2018-08-07 18:36:43 +0000

    removing errant comment

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (neu)
 		{
 			neu->n = n;
-			//neu->next = (*head)->next;
 			if (idx == 0)
 			{
 				neu->next = *head;
