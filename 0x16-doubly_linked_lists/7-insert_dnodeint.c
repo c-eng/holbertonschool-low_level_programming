7e57c4260c15d1128dba332083c2d70625d8e529
c-eng <ceng@cca.edu>
2018-09-17 02:52:52 +0000

    adding task for online checking

---
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a node at index into a doubly linked list
+ *
+ * @h: head of list
+ * @idx: index to insert at
+ * @n: value of node to insert
+ *
+ * Return: address of new node or NULL upon failure
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int index = 0;
+	dlistint_t *add = NULL, *strider = NULL;
+
+	if (!h)
+		return (NULL);
+	add = malloc(sizeof(dlistint_t));
+	if (!add)
+		return (NULL);
+	add->n = n;
+	strider = *h;
+	while (strider && index < idx)
+	{
+		index += 1;
+		strider = strider->next;
+	}
+	if (index == idx)
+	{
+		add->prev = strider->prev;
+		add->next = strider;
+		if (strider->prev)
+			strider->prev->next = add;
+		else
+			*h = add;
+		if (strider)
+			strider->prev = add;
+		return (add);
+	}
+	return (NULL);
+}
