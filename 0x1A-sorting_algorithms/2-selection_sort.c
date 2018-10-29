e5b503956cb9515c125e250ecf2a153c4f05dc3f
Hunt6666 <489@holbertonschool.com>
2018-10-28 19:36:47 -0700

    got it working muhahahahahahahaha

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -9,30 +9,36 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, j, k, l;
 	int temp, t;
 
 	for (i = 0; i < size; i++)
 	{
 		t = array[i];
+		temp = array[i];
 		for (j = i + 1; j < size; j++)
 		{
 			if (t > array[j])
 			{
-				temp = array[j];
+				/*	temp = array[j];
 				array[j] = t;
-				t = temp;
-				array[i] = temp;
+				t = temp;*/
+				t = array[j];
+				l = j;
 			}
 		}
-		for (k = 0; k < size; k++)
+		if (t != temp)
 		{
-			printf("%d", array[k]);
-			if (k != size - 1)
-				printf(", ");
-			else
-				printf("\n");
+			array[l] = temp;
+			array[i] = t;
+			for (k = 0; k < size; k++)
+			{
+				printf("%d", array[k]);
+				if (k != size - 1)
+					printf(", ");
+				else
+					printf("\n");
+			}
 		}
-		array[i] = t;
 	}
 }
