c279a1e0cb505fc8f842199b89fa0d8a0eb113aa
Hunt6666 <489@holbertonschool.com>
2018-10-28 18:03:55 -0700

    not done

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ *selection_sort - uses selection sort on an array of ints
+ *@array: array of ints
+ *@size: the size of the array
+ *Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp, t;
+
+	for (i = 0; i < size; i++)
+	{
+		t = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (t > array[j])
+			{
+				temp = array[j];
+				array[j] = t;
+				t = temp;
+				array[i] = temp;
+				for (k = 0; k < size; k++)
+				{
+					printf("%d", array[k]);
+					if (k != size - 1)
+						printf(", ");
+					else
+						printf("\n");
+				}
+				array[i] = t;
+			}
+		}
+	}
+}
