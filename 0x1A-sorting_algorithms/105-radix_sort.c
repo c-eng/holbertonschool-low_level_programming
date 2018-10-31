3b9f7e294752948c19188b013064254045a33340
Hunt6666 <489@holbertonschool.com>
2018-10-31 14:41:52 -0700

    i does not work I broke a size_t

--- 0x1A-sorting_algorithms/105-radix_sort.c
+++ 0x1A-sorting_algorithms/105-radix_sort.c
@@ -50,17 +50,18 @@ int count_bits(unsigned int n)
  *Return: void
  */
 
-void make_array(int *array, size_t size, size_t loc)
+void make_array(int *array, size_t size, unsigned int loc)
 {
 	int new_array[sizeof(array)];
 	size_t i, j = 0;
-	int count = 0;
+	size_t count = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		if (get_bit((unsigned int) array[i], loc) == 0)
 			count++;
 	}
+	printf("test 1\n");
 	for (i = 0; i < size; i++)
 	{
 		if (get_bit((unsigned int) array[i], loc) == 0)
@@ -74,11 +75,13 @@ void make_array(int *array, size_t size, size_t loc)
 			count++;
 		}
 	}
+	printf("test 2\n");
 	for (i = 0; i < size; i++)
 	{
 		array[i] = new_array[i];
 	}
 	print_array(array, size);
+	printf("test 3\n");
 }
 
 
@@ -93,8 +96,8 @@ void make_array(int *array, size_t size, size_t loc)
 void radix_sort(int *array, size_t size)
 {
 	size_t i;
-	unsigned int biggest = array[0];
 	unsigned int j;
+	unsigned int biggest = array[0];
 
 	for (i = 0; i < size; i++)
 	{
@@ -102,11 +105,16 @@ void radix_sort(int *array, size_t size)
 			biggest = array[i];
 	}
 	biggest = count_bits(biggest);
-	for (j = 0; j < (unsigned int) biggest; j++)
+	i = 0;
+	while (i < (size_t) biggest)
 	{
 		printf("%d\n", biggest);
 		printf("befor make array\n");
+		j = (unsigned int) i + 1;
+		j--;
+		printf("%lu\n", i);
 		make_array(array, size, j);
 		printf("after make array\n");
+		i++;
 	}
 }
