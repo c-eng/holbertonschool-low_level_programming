56cadc715f1fc1c53ad3e5ee49c81401feb588b2
c-eng <ceng@cca.edu>
2018-11-17 01:32:27 +0000

    Modifying for exterior perimiter

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -10,12 +10,24 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                if i > 0:
+                    if grid[i - 1][j] == 0:
+                        perimiter += 1
+                else:
                     perimiter += 1
-                if grid[i + 1][j] == 0:
+                if i < len(grid) - 1:
+                    if grid[i + 1][j] == 0:
+                        perimiter += 1
+                else:
                     perimiter += 1
-                if grid[i][j - 1] == 0:
+                if j > 0:
+                    if grid[i][j - 1] == 0:
+                        perimiter += 1
+                else:
                     perimiter += 1
-                if grid[i][j + 1] == 0:
+                if j < len(grid[i]) - 1:
+                    if grid[i][j + 1] == 0:
+                        perimiter += 1
+                else:
                     perimiter += 1
     return perimiter
